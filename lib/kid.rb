79d28e774fd13bacc48b3720e54b7d8d506990ae
Shaina C <scailles23@gmail.com>
2021-01-10 07:34:11 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,23 +3,19 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 
-class Kid 
-   extend
-  FancyDance::ClassMethods 
-  include 
-  FancyDance::InstanceMethods
+class Kid
+  include Dance
+  extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
   
-  attr_accessor :name 
+  
+  attr_accessor :name
   
   
   def initialize(name)
     @name = name
   end
-  
-  
-  
-  
-  
   
   
 end
